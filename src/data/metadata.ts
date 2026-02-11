9933238d3a749386fa28c331814bbbce32ac9481
maitamgk <maitamit062005@gmail.com>
2026-02-11 12:49:00 +0700

    docs: add API documentation comments to types

--- src/data/metadata.ts
+++ src/data/metadata.ts
@@ -3,4 +3,5 @@
 // Update #1: docs: update project description in README (2026-02-10 14:48:00)
 // Update #2: style: format import statements in App.tsx (2026-02-11 16:19:00)
 // Update #3: chore: add .editorconfig for consistent coding style (2026-02-11 12:28:00)
+// Update #4: docs: add API documentation comments to types (2026-02-11 12:49:00)
 
