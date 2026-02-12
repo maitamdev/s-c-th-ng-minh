aa80b7a5083b8b411685c18b191a669dcaf9edce
maitamgk <maitamit062005@gmail.com>
2026-02-12 10:53:00 +0700

    style: organize CSS custom properties in index.css

--- src/data/metadata.ts
+++ src/data/metadata.ts
@@ -5,4 +5,5 @@
 // Update #3: chore: add .editorconfig for consistent coding style (2026-02-11 12:28:00)
 // Update #4: docs: add API documentation comments to types (2026-02-11 12:49:00)
 // Update #5: refactor: extract station status constants (2026-02-12 08:31:00)
+// Update #6: style: organize CSS custom properties in index.css (2026-02-12 10:53:00)
 
