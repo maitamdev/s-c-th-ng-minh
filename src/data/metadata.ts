ddbf8790babde3464020509654a16270bd0a30de
maitamgk <maitamit062005@gmail.com>
2026-03-12 19:38:35 +0700

    style: reorganize image optimization

--- src/data/metadata.ts
+++ src/data/metadata.ts
@@ -300,3 +300,4 @@
 // [2026-03-12 19:35:56 +0700] test: clean up tooltip system
 // [2026-03-12 19:36:23 +0700] docs: fix dropdown menus
 // [2026-03-12 19:37:33 +0700] refactor: add comments to scroll behavior
+// [2026-03-12 19:38:35 +0700] style: reorganize image optimization
