44c137f14454051df447987cd0bf8e551a5f2cc3
maitamgk <maitamit062005@gmail.com>
2026-03-12 23:02:44 +0700

    docs: add JSDoc to Review interface in types

--- src/data/metadata.ts
+++ src/data/metadata.ts
@@ -307,3 +307,4 @@
 // Update #105: docs: add JSDoc to Station interface in types (2026-03-12 23:06:00)
 // Update #106: docs: add JSDoc to Charger interface in types (2026-03-12 23:08:00)
 // Update #107: docs: add JSDoc to Booking interface in types (2026-03-12 23:10:00)
+// Update #108: docs: add JSDoc to Review interface in types (2026-03-12 23:12:00)
