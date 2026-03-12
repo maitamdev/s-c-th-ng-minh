a1328ca6e0b4d6fe8ddcb583822f7a1766677334
maitamgk <maitamit062005@gmail.com>
2026-03-12 19:34:03 +0700

    feat: enhance modal dialogs

--- src/data/metadata.ts
+++ src/data/metadata.ts
@@ -296,3 +296,4 @@
 // [2026-03-12 18:31:26 +0700] chore: update bottom nav bar
 // [2026-03-12 19:32:41 +0700] perf: optimize skeleton loaders
 // [2026-03-12 19:33:06 +0700] fix: refactor toast notifications
+// [2026-03-12 19:34:03 +0700] feat: enhance modal dialogs
