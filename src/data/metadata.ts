a0ce16450e7b02eb55d8087ee5512b440be2d449
maitamgk <maitamit062005@gmail.com>
2026-03-12 08:07:34 +0700

    test: add comments to pricing page

--- src/data/metadata.ts
+++ src/data/metadata.ts
@@ -209,3 +209,4 @@
 // [2026-03-12 08:04:51 +0700] perf: enhance map integration
 // [2026-03-12 08:05:36 +0700] fix: clean up AI recommendations
 // [2026-03-12 08:06:30 +0700] feat: fix review system
+// [2026-03-12 08:07:34 +0700] test: add comments to pricing page
