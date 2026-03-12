1c77cd33c1165622cd8b5f8aa23ccd76a630c632
maitamgk <maitamit062005@gmail.com>
2026-03-12 09:12:43 +0700

    perf: streamline SEO metadata

--- src/data/metadata.ts
+++ src/data/metadata.ts
@@ -214,3 +214,4 @@
 // [2026-03-12 09:09:36 +0700] refactor: simplify dashboard layout
 // [2026-03-12 09:10:57 +0700] style: document operator panel
 // [2026-03-12 09:11:44 +0700] chore: polish PWA config
+// [2026-03-12 09:12:43 +0700] perf: streamline SEO metadata
