47246c31322c342b0e2239081b25f757588f5139
maitamgk <maitamit062005@gmail.com>
2026-03-12 23:02:57 +0700

    chore: document ESLint rule overrides rationale

--- src/data/metadata.ts
+++ src/data/metadata.ts
@@ -385,3 +385,4 @@
 // Update #183: chore: document Vite alias configuration (2026-03-13 01:42:00)
 // Update #184: chore: document PostCSS plugin chain (2026-03-13 01:44:00)
 // Update #185: chore: document Tailwind configuration decisions (2026-03-13 01:46:00)
+// Update #186: chore: document ESLint rule overrides rationale (2026-03-13 01:48:00)
