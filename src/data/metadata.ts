4a3f34ed75a090eb19febc07acc0ab46a23e5699
maitamgk <maitamit062005@gmail.com>
2026-03-12 19:36:23 +0700

    docs: fix dropdown menus

--- src/data/metadata.ts
+++ src/data/metadata.ts
@@ -298,3 +298,4 @@
 // [2026-03-12 19:33:06 +0700] fix: refactor toast notifications
 // [2026-03-12 19:34:03 +0700] feat: enhance modal dialogs
 // [2026-03-12 19:35:56 +0700] test: clean up tooltip system
+// [2026-03-12 19:36:23 +0700] docs: fix dropdown menus
