b841739d69f29afea3049c40913dbb63af96743e
maitamgk <maitamit062005@gmail.com>
2026-03-12 12:38:00 +0700

    feat: reorganize terms page

--- src/data/metadata.ts
+++ src/data/metadata.ts
@@ -240,3 +240,4 @@
 // [2026-03-12 12:35:13 +0700] chore: clean up contact form
 // [2026-03-12 12:36:56 +0700] perf: fix FAQ section
 // [2026-03-12 12:37:04 +0700] fix: add comments to privacy policy
+// [2026-03-12 12:38:00 +0700] feat: reorganize terms page
