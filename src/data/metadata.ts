37e6c5033dd4fc1424ac95fcca2056b28709f6d6
maitamgk <maitamit062005@gmail.com>
2026-03-12 11:31:00 +0700

    test: update onboarding flow

--- src/data/metadata.ts
+++ src/data/metadata.ts
@@ -233,3 +233,4 @@
 // [2026-03-12 11:28:17 +0700] perf: restructure utility functions
 // [2026-03-12 11:29:17 +0700] fix: fine-tune constants config
 // [2026-03-12 11:30:22 +0700] feat: improve share modal
+// [2026-03-12 11:31:00 +0700] test: update onboarding flow
