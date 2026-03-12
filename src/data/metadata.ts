17391b7f933c3b86794281c3b9a9dbd0e6e072bc
maitamgk <maitamit062005@gmail.com>
2026-03-12 08:02:01 +0700

    style: optimize auth system

--- src/data/metadata.ts
+++ src/data/metadata.ts
@@ -204,3 +204,4 @@
 // [] chore: simplify font loading
 // [2026-03-12 08:00:43 +0700] docs: improve station search module
 // [2026-03-12 08:01:04 +0700] refactor: update booking flow
+// [2026-03-12 08:02:01 +0700] style: optimize auth system
